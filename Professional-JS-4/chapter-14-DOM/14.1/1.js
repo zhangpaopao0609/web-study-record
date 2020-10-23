9dd0af58a23721a994b40bc581480bf0645d74da
arrow_zb <arrow_zb@outlook.com>
2020-10-23 20:42:41 +0800

    feat: add Dom

---
+++ Professional-JS-4/chapter-14-DOM/14.1/1.js
@@ -0,0 +1,34 @@
+console.log(document.documentElement);
+console.log(document.firstChild);
+console.log(document.childNodes);
+console.log(document.body);
+console.log(document.head);
+console.log(document.doctype);
+console.log(document.nodeType);
+console.log(document.nodeName);
+
+console.log(document.childNodes);
+console.log(document.title);
+
+console.log(document.URL);
+console.log(document.domain);
+console.log(document.referrer);
+
+
+console.log('a', document.anchors);
+
+console.log(document.implementation.hasFeature('html', '1.0'));
+
+document.writeln('arrow');
+document.writeln('arrow');
+document.writeln(Date.now());
+document.write(1)
+
+window.onload = function() {
+  document.write(1)
+  document.write(1)
+}
+// setInterval(() => {
+//   document.write(Date.now())
+// },1)
+
