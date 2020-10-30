59014d17ce9333762e3746c6b6ebb1d604c6e019
zhangpaopao <zhangpaopao0609@gmail.com>
2020-10-30 14:09:57 +0800

    feat: update-sequelize

--- OpenClass/NODE/Node-day04-DataPersistence-Mysql/mysql/sequelize.js
+++ OpenClass/NODE/Node-day04-DataPersistence-Mysql/mysql/sequelize.js
@@ -1,3 +1,5 @@
+const { first } = require('lodash');
+
 (async () => {
   const { Sequelize } = require('sequelize');
   const sequelize = new Sequelize('arrow', 'root', '123456', {
@@ -7,10 +9,34 @@
 
   // 定义模型
   const Fruit = sequelize.define('Fruit', {
+    id: {
+      type: Sequelize.DataTypes.UUID,
+      defaultValue: Sequelize.DataTypes.UUIDV1,
+      primaryKey: true
+    },
     name: { type: Sequelize.STRING(20), allowNull: false },
     price: { type: Sequelize.FLOAT, allowNull: false },
     stock: { type: Sequelize.INTEGER, defaultValue: 0 }
   });
 
-  const ret = await Fruit.sync();
+  const ret = await Fruit.sync({ force: true });
+  // const ret = await Fruit.sync();
+
+  const TABLE_INSERT = await Fruit.create({
+    name: '香蕉',
+    price: 3.5
+  });
+  // console.log(TABLE_INSERT);
+
+  const TABLE_UPDATE = await Fruit.update({
+    price: 4
+  }, {
+    where: { name: '香蕉' }
+  });
+
+  const Op = Sequelize.Op;
+  const TABLE_SELECT = await Fruit.findAll({
+    where: { price: { [Op.lt]: 5, [Op.gt]: 2 } }
+  });
+
 })();
