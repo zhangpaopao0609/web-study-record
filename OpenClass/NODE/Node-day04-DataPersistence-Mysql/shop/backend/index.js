e569a61208b7b64e49d9968aed67134b3e0444dc
zhangpaopao <zhangpaopao0609@gmail.com>
2020-10-31 10:57:57 +0800

    feat: add-backend

--- OpenClass/NODE/Node-day04-DataPersistence-Mysql/shop/backend/index.js
+++ OpenClass/NODE/Node-day04-DataPersistence-Mysql/shop/backend/index.js
@@ -66,4 +66,109 @@ router.get('/admin/products', async (ctx, next) => {
   ctx.body = { prods: products };
 });
 
+/**
+ * 创建商品
+ */
+router.post('/admin/product', async (ctx, next) => {
+  const body = ctx.request.body;
+  const res = await ctx.user.createProduct(body);
+  ctx.body = { success: true };
+});
+
+/**
+ * 删除商品
+ */
+router.delete('/admin/product/:id', async (ctx, next) => {
+  const id = ctx.params.id;
+  const res = await Product.destroy({
+    where: { id }
+  });
+  ctx.body = { success: true };
+});
+
+/**
+ * 查询购物车
+ */
+router.get('/cart', async (ctx, next) => {
+  const cart = await ctx.user.getCart();
+  const products = await cart.getProducts();
+  ctx.body = { products };
+});
+
+/**
+ * 添加购物车
+ */
+router.post('/cart', async (ctx, next) => {
+  const { body } = ctx.request;
+  const { id } = body;
+  let newQty = 1;
+  const cart = await ctx.user.getCart();
+  const products = await cart.getProducts({
+    where: { id }
+  });
+  let product;
+  // 购物车中已存在此商品
+  if(products.length > 0) {
+    product = products[0];
+  };
+  if(product) {
+    // 这个产品在购物车中的数量
+    const oldQty = product.cartItem.quantity;
+    newQty = oldQty + 1;
+  }else {
+    product = await Product.findByPk(id);
+  };
+
+  await cart.addProduct(product, {
+    through: { quantity: newQty }
+  });
+
+  ctx.body = { success: true };
+});
+
+/**
+ * 添加订单
+ */
+router.post('/orders', async ctx => {
+  const cart = await ctx.user.getCart();
+  const products = await cart.getProducts();
+  const order = await ctx.user.createOrder();
+  const result = await order.addProduct(
+    products.map(p => {
+      p.orderItem = {
+        quantity: p.cartItem.quantity
+      };
+      return p;
+    })
+  );
+  // 清空购物车
+  await cart.setProducts(null);
+  ctx.body = { success: true };
+});
+
+/**
+ * 删除购物车
+ */
+router.delete('/cartItem/:id', async ctx => {
+  const id = ctx.params.id;
+  const cart = await ctx.user.getCart();
+  const products = await cart.getProducts({
+    where: { id }
+  });
+  const product = products[0];
+  await product.cartItem.destroy();
+  ctx.body = { success: true };
+})
+
+/**
+ * 查询订单
+ */
+router.get('/orders', async ctx => {
+  const orders = await ctx.user.getOrders({ 
+    include: ['products'],
+    order: [['id', 'DESC']],
+  });
+  ctx.body = { orders } 
+})
+
 app.use(router.routes());
