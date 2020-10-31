eb1fe9493dfa80e0ee7c6687297622f3530546ae
zhangpaopao <zhangpaopao0609@gmail.com>
2020-10-31 10:57:43 +0800

    feat: add-frontend

--- OpenClass/NODE/Node-day04-DataPersistence-Mysql/shop/frontend/vue.config.js
+++ OpenClass/NODE/Node-day04-DataPersistence-Mysql/shop/frontend/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     proxy: {
       "/api": {
         //代理api
-        target: "http://localhost:3000/", //服务器api地址
+        target: "http://localhost:6090/", //服务器api地址
         changeOrigin: true, //是否跨域
         ws: true, // proxy websockets
         pathRewrite: {
