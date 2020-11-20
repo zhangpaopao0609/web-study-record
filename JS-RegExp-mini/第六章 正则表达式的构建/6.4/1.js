9b604f2f3245c2005fac0eeb451e26414081ade0
zhangpaopao <zhangpaopao0609@gmail.com>
2020-11-20 08:58:54 +0800

    feat: add analysize

---
+++ JS-RegExp-mini/第六章 正则表达式的构建/6.4/1.js
@@ -0,0 +1,8 @@
+const regex = /\d+/g;
+
+const str = "123abc34def";
+
+console.log(regex.lastIndex, regex.exec(str));
+console.log(regex.lastIndex, regex.exec(str));
+console.log(regex.lastIndex, regex.exec(str));
+console.log(regex.lastIndex, regex.exec(str));
