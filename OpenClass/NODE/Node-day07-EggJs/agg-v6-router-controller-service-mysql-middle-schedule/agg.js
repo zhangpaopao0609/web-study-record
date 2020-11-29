5705bd6582c2404453432531d3da5752c432436c
arrow_zb <arrow_zb@outlook.com>
2020-11-29 17:05:41 +0800

    feat: agg-v6

---
+++ OpenClass/NODE/Node-day07-EggJs/agg-v6-router-controller-service-mysql-middle-schedule/agg.js
@@ -0,0 +1,31 @@
+const Koa = require('koa');
+const { 
+  initRouter, 
+  initController, 
+  initService,
+  loadConfig,
+  initSchedule
+} = require('./loader/agg-loader');
+
+class agg {
+  constructor() {
+    this.$app = new Koa();
+    // 加载配置项
+    loadConfig(this);
+    initSchedule();
+    this.$service = initService(this);
+    this.$ctrl = initController(this);
+    this.$router = initRouter(this);
+    this.$app.use(this.$router.routes());
+  }
+
+  start() {
+    const port = 6090;
+    this.$app.listen(port, err => {
+      if(err) throw err;
+      console.log(`app start at ${port}`);
+    });
+  }
+};
+
+module.exports = agg;
---
+++ OpenClass/NODE/Node-day07-EggJs/agg-v6-router-controller-service-mysql-middle-schedule/loader/agg-loader.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+const path = require('path');
+const Router = require('koa-router');
+
+// 读取指定文件
+function load(dir, cb) {
+  // 获取绝对路径
+  const url = path.resolve(__dirname, dir);
+
+  // 读文件
+  const files = fs.readdirSync(url);
+
+  // 遍历文件夹
+  files.forEach(filename => {
+    filename = filename.replace('.js', '');
+    const file = require(`${url}/${filename}`);
+    // 处理逻辑
+    cb(filename, file);
+  })
+};
+
+function initRouter(app) {
+  const router = new Router();
+  load('../routes', (filename, routes) => {
+    // 路由的前缀
+    const prefix = filename === 'index' ? '' : `/${filename}`;
+    routes = routes instanceof Function ? routes(app) : routes; 
+    // 遍历对象
+    Object.keys(routes).forEach(key => {
+      let [method, path] = key.split(' ');
+      path = path === '/' ? '' : path;
+      console.log(`正在映射地址： ${method.toLocaleUpperCase()} ${prefix}${path}`);
+      router[method](prefix + path, async ctx => {
+        // 挂载上下文到 app
+        app.ctx = ctx; 
+        // 路由处理接收 app
+        await routes[key](app);
+      })
+    });
+  });
+  return router;
+};
+
+function initController(app) {
+  const controllers = {};
+  // 读取ctrl
+  load('../controller', (filename, controller) => {
+    controllers[filename] = controller(app);
+  });
+  return controllers;
+}
+
+function initService() {
+  const services = {};
+  // 读取ctrl
+  load('../service', (filename, service) => {
+    services[filename] = service;
+  });
+  return services;
+};
+
+function loadConfig(app) {
+  const Sequelize = require('sequelize');
+  load('../config', (filename, conf) => {
+    if(conf.db) {
+      app.$db = new Sequelize(conf.db);
+      // 加载模型
+      app.$model = {};
+      load('../model', (filename, { schema, options }) => {
+        app.$model[filename] = app.$db.define(filename, schema, options);
+      });
+      app.$db.sync();
+    };
+    if(conf.middleware) {
+      conf.middleware.forEach(mid => {
+        const midPath = path.resolve(`${__dirname}`,'../middleware', mid);
+        console.log(midPath);
+        app.$app.use(require(midPath));
+      })
+    }
+  })
+};
+
+function initSchedule() {
+  const schedule = require('node-schedule');
+  load('../schedule', (filename, scheduleConfig) => {
+    schedule.scheduleJob(scheduleConfig.interval, scheduleConfig.handler);
+  })
+}
+
+module.exports = {
+  initRouter,
+  initController,
+  initService,
+  loadConfig,
+  initSchedule
+};
