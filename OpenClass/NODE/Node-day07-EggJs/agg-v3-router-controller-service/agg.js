603facb11b3d94298a0b0010e39c9717426dac52
zhangpaopao <zhangpaopao0609@gmail.com>
2020-11-29 17:04:53 +0800

    feat: agg-v3

---
+++ OpenClass/NODE/Node-day07-EggJs/agg-v3-router-controller-service/agg.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const { 
+  initRouter, 
+  initController, 
+  initService 
+} = require('./loader/agg-loader');
+
+class agg {
+  constructor() {
+    this.$app = new Koa();
+    this.$service = initService(this);
+    this.$ctrl = initController(this);
+    this.$router = initRouter(this);
+    this.$app.use(this.$router.routes());
+  }
+
+  start() {
+    const port = 6090;
+    this.$app.listen(port, err => {
+      if(err) throw err;
+      console.log(`app start at ${port}`);
+    });
+  }
+};
+
+module.exports = agg;
---
+++ OpenClass/NODE/Node-day07-EggJs/agg-v3-router-controller-service/controller/home.js
@@ -0,0 +1,9 @@
+module.exports = app => ({
+  index: async ctx => {
+    const name = await app.$service.user.getName();
+    app.ctx.body = `user ctrl ${name}`
+  },
+  detail: ctx => {
+    app.ctx.body = "详情⻚⾯ Ctrl...";
+  }
+});
