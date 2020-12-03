f9f20fbba09cc3593c3ef8ed1746ec8b5df63c56
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-04 07:16:53 +0800

    feat: middlewares more

--- OpenClass/NODE/Node-day09-TS-Decorate/ts-server/src/routes/users.ts
+++ OpenClass/NODE/Node-day09-TS-Decorate/ts-server/src/routes/users.ts
@@ -1,21 +1,38 @@
 import * as Koa from "koa";
-import { get, post } from "../utils/decorate";
+import { get, post, middlewares } from "../utils/decorate";
 
-const users = [{
-  name: 'arrow'
-}];
-
+@middlewares([
+  async function guard(ctx: Koa.Context, next: () => Promise<any>) {
+    if(ctx.header.token) {
+      await next();
+    }else {
+      throw '请登录！'
+    }
+  }
+])
 export default class User {
   @get("/user")
   public list(ctx: Koa.Context) {
     ctx.body = {
       ok: 1,
-      data: users
+      data: {
+        name: 'arrow'
+      }
     }
   }
-  @post('/user')
+  @post('/user', {
+    middlewares: [
+      async function validation(ctx: Koa.Context, next: () => Promise<any>) {
+        // 用户名必须
+        const name = ctx.request.body.name;
+        if(!name) {
+          throw "请输入用户名！"
+        }
+        await next();
+      }
+    ]
+  })
   public add(ctx: Koa.Context) {
-    users.push(ctx.request.body);
     ctx.body = { ok: 1 };
   }
 }
--- OpenClass/NODE/Node-day09-TS-Decorate/ts-server/src/utils/decorate.ts
+++ OpenClass/NODE/Node-day09-TS-Decorate/ts-server/src/utils/decorate.ts
@@ -2,6 +2,7 @@ import * as glob from 'glob';
 import * as Koa from 'koa';
 import * as KoaRouter from 'koa-router';
 import * as path from 'path';
+import { debug } from 'console';
 
 type HTTPMethod = 'get' | 'put' | 'del' | 'post' | 'patch';
 type LoadOptions = {
@@ -23,12 +24,36 @@ type RouteOptions = {
 
 const router = new KoaRouter();
 
-const decorate = (method: HTTPMethod, path: string, options: RouteOptions, router: KoaRouter) => {
+const decorate = (method: HTTPMethod, path: string, options: RouteOptions = {}, router: KoaRouter) => {
   return (target, property, descriptor) => {
-    const url = options && options.prefix ? options.prefix + path : path;
-    return router[method](url, descriptor.value);
+    // 因为程序中方法装饰器会比类装饰器先执行，但是我们这里想要的刚好相反
+    // 那么就让方法装饰器异步执行，类装饰器同步执行就可以了
+    // 晚一拍执行
+    process.nextTick(() => {
+      // 添加中间件数组
+      const middlewares = [];
+      console.log('target---func', target);
+      if(target.middlewares) {
+        middlewares.push(...target.middlewares);
+      };
+  
+      if(options.middlewares) {
+        middlewares.push(...options.middlewares);
+      };
+      middlewares.push(descriptor.value);
+  
+      const url = options && options.prefix ? options.prefix + path : path;
+      return router[method](url, ...middlewares);
+    })
   }
 };
+
+export const middlewares = (middlewares: Koa.Middleware[]) => {
+  return target => {
+    console.log('target---class', target);
+    target.prototype.middlewares = middlewares;
+  }
+}
 
 const method = method => (path: string, options?: RouteOptions) => decorate(method, path, options, router);
 
@@ -40,9 +65,7 @@ export const patch = method('patch');
 
 export const load = (folder: string, options: LoadOptions = {}): KoaRouter => {
   const extname = options.extname || '.{js,ts}';
-  glob.sync(path.join(folder, `./**/*${extname}`)).forEach(item => {
-    require(item);
-  });
+  glob.sync(path.join(folder, `./**/*${extname}`)).forEach(item => require(item));
   return router;
 };
 
