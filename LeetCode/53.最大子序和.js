46e103e40c86b957e22cf09e763b5c3199de32dc
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-12 18:54:31 +0800

    update: 最大子序和

--- LeetCode/53.最大子序和.js
+++ LeetCode/53.最大子序和.js
@@ -10,13 +10,18 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 贪心2
 const maxSubArray = function(nums) {
-  let pre = 0, maxAns = nums[0];
-  nums.forEach((x) => {
-      pre = Math.max(pre + x, x);
-      maxAns = Math.max(maxAns, pre);
-  });
-  return maxAns;
+  let max = current = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if(current < 0) {
+      current = nums[i];
+    }else {
+      current += nums[i];
+    }
+    max = Math.max(current, max);
+  }
+  return max;
 };
 // @lc code=end
 
@@ -31,3 +36,19 @@ const maxSubArray_1 = function(nums) {
   return maxAns;
 };
 
+// 贪心1
+const maxSubArray_2 = function(nums) {
+  let max = nums[0];
+  let pre = nums[0];
+  let now = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if(pre < 0) {
+      now = nums[i];
+    }else {
+      now = pre + nums[i];
+    }
+    pre = now;
+    max = Math.max(now, max);
+  }
+  return max;
+};
