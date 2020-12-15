9130bdcc81cd83e1f385ad74d044f72077f76302
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-15 20:27:34 +0800

    feat: 69-x的平方根

--- LeetCode/69.x-的平方根.js
+++ LeetCode/69.x-的平方根.js
@@ -10,7 +10,23 @@
  * @param {number} x
  * @return {number}
  */
+
+// 牛顿法
 const mySqrt = x => {
+  if(x === 0) return 0;
+  let x0 = 0, x1 = x;
+  while(Math.abs(x1-x0) > 10E-2) {
+  // while(Math.abs(x1-x0) > 10E-6) {
+    x0 = x1;
+    x1 = (x0 + (x / x0)) * 0.5
+  }
+  return  parseInt(x1);
+};
+// @lc code=end
+
+// 二分法
+// 关键在于临界值
+const mySqrt_1 = x => {
   if(x === 0) return 0;
   if(x === 1) return 1;
 
@@ -30,5 +46,26 @@ const mySqrt = x => {
 
   return Math.min(left, right);
 };
-// @lc code=end
 
+// 清爽的二分法
+const mySqrt_3 = x => {
+  let l = 0, r = x, ans = -1;
+  while(l <= r) {
+    const mid = (l + r) >> 1;
+    if(mid*mid <= x) {
+      ans = mid;
+      l = mid + 1;
+    }else {
+      r = mid - 1;
+    }
+  }
+  return ans;
+};
+
+// 数学法
+// 根号x 其实就等于x的1/2方，x又等于e的lnx次幂，所以根号x也就等于e的1/2倍lnx次幂
+const mySqrt_2 = x => {
+  if(x === 0) return 0;
+  let ans = parseInt(Math.exp(0.5 * Math.log(x)));
+  return (ans + 1) * (ans + 1) <= x ? ans + 1 : ans
+};
