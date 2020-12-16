a1da53a1e58ecec314b75222ab553c9137acc129
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-16 20:15:27 +0800

    update: 101-对称二叉树

--- LeetCode/101.对称二叉树.js
+++ LeetCode/101.对称二叉树.js
@@ -17,10 +17,26 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-const check = (p, q) => {
-  if(!p && !q) return true;
-  if(!p || !q) return false;
-  return p.val === q.val && check(p.left, q.right) && check(p.right, q.left);
+const check = (u, v) => {
+  const q = [];
+  q.push(u);
+  q.push(v);
+
+  while(q.length) {
+    u = q.shift();
+    v = q.shift();
+
+    if(!u && !v) continue;
+    if((!u || !v) || (u.val !== v.val)) return false;
+
+    q.push(u.left);
+    q.push(v.right);
+
+    q.push(u.right);
+    q.push(v.left);
+  };
+
+  return true;
 }
 
 const isSymmetric = root => {
@@ -29,3 +45,14 @@ const isSymmetric = root => {
 };
 // @lc code=end
 
+// 递归
+const check_1 = (p, q) => {
+  if(!p && !q) return true;
+  if(!p || !q) return false;
+  return p.val === q.val && check(p.left, q.right) && check(p.right, q.left);
+}
+
+const isSymmetric_1 = root => {
+  if(!root) return true;
+  return check(root.left, root.right);
+};
