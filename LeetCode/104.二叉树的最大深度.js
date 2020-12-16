36e4e4ec116f6b339094d92f9977b85385d5d749
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-16 20:27:58 +0800

    feat: 104-二叉树的最大深度

---
+++ LeetCode/104.二叉树的最大深度.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = root => {
+  if(!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1; 
+};
+// @lc code=end
+
