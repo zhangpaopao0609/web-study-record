85f33c0b901a568d8d301c74c119398357304288
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-17 20:07:54 +0800

    update: 118-杨辉三角

--- LeetCode/118.杨辉三角.js
+++ LeetCode/118.杨辉三角.js
@@ -11,6 +11,20 @@
  * @return {number[][]}
  */
 const generate = numRows => {
+  const ret = [];
+  for (let i = 0; i < numRows; i++) {
+    const row = Array(i+1).fill(1);
+    // 这里挺巧妙的，用j直接略过了i为0和1
+    for (let j = 1; j < i; j++) {
+      row[j] = ret[i-1][j-1] + ret[i-1][j];
+    };
+    ret.push(row);
+  };
+  return ret;
+};
+// @lc code=end
+
+const generate_1 = numRows => {
   if(!numRows) return [];
   if(numRows === 1) return [[1]];
   if(numRows === 2) return [[1], [1, 1]];
@@ -25,5 +39,3 @@ const generate = numRows => {
   }
   return q;
 };
-// @lc code=end
-
