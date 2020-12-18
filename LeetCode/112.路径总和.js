eca580bde06008bb218118e0b6122df5943ce66e
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-18 21:27:47 +0800

    update: 112-路径总和

--- LeetCode/112.路径总和.js
+++ LeetCode/112.路径总和.js
@@ -18,6 +18,16 @@
  * @param {number} sum
  * @return {boolean}
  */
+
+const hasPathSum = (root, sum) => {
+  if(!root) return false;
+  if(!root.left && !root.right) return sum === root.val;
+  return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val)
+};
+// @lc code=end
+
+
+// BFS 方法
 const BFS = tree => {
   if(!tree) return [];
   const q = [];
@@ -48,8 +58,39 @@ const BFS = tree => {
   return res;
 }
 
-const hasPathSum = (root, sum) => {
+const hasPathSum_1 = (root, sum) => {
   const res = BFS(root);
   return res.includes(sum);
 };
-// @lc code=end
+
+
+
+// BFS 2
+const hasPathSum_2 = (root, sum) => {
+  if(!root) return false;
+  const q = [];
+  q.push(root);
+
+  while(q.length) {
+    // 上一层的长度
+    let size = q.length;
+
+    while(size > 0) {
+      size--;
+      const next = q.shift();
+      if(!next.left && !next.right) {
+        if(next.val === sum) return true;
+        continue;
+      }
+      if(next.left) {
+        next.left.val += next.val;
+        q.push(next.left);
+      }
+      if(next.right) {
+        next.right.val += next.val;
+        q.push(next.right);
+      }
+    }
+  };
+  return false;
+};
