e299b7df098e7dc1421e31e988bfccc82975df83
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-22 06:53:34 +0800

    update: 189-旋转数组

--- LeetCode/189.旋转数组.js
+++ LeetCode/189.旋转数组.js
@@ -12,28 +12,29 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-// 使用环状替换
-const rotate = (nums, k) => {
-  let l = count = nums.length;
-  k = k % l;
-  let prev = next = record = 0;
-  let prevNum = nums[0], nextNum = null;
-  
-  while(count) {
-    next = (prev+k) % l;
-    nextNum = nums[next];
-    nums[next] = prevNum;
-    if(next === record){
-      record++; 
-      prev = record;
-      prevNum = nums[record];
-    }else{
-      prev = next;
-      prevNum = nextNum; 
-    }
-    count--;
+// 使用反转
+// 这个方法基于这个事实： 当旋转数组 k 次， k % n 个尾部元素会被移动到头部，剩下的元素会被向后移动
+const reverse = (nums, start, end) => {
+  while(start < end) {
+    const temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
   }
+}
+
+const rotate = (nums, k) => {
+  const len = nums.length;
+  k %= len;
+  reverse(nums, 0, len-1);
+  reverse(nums, 0, k-1);
+  reverse(nums, k, len-1);
 };
+// Accepted
+// 35/35 cases passed (80 ms)
+// Your runtime beats 98.88 % of javascript submissions
+// Your memory usage beats 27.88 % of javascript submissions (39.6 MB)
 // @lc code=end
 
 
@@ -95,4 +96,29 @@ const rotate_4 = (nums, k) => {
   for (let i = 0; i < len; i++) {
     nums[i] = newOne[i];
   } 
+};
+
+
+// -------------------------------------------------
+// 使用环状替换
+const rotate = (nums, k) => {
+  let l = count = nums.length;
+  k = k % l;
+  let prev = next = record = 0;
+  let prevNum = nums[0], nextNum = null;
+  
+  while(count) {
+    next = (prev+k) % l;
+    nextNum = nums[next];
+    nums[next] = prevNum;
+    if(next === record){
+      record++; 
+      prev = record;
+      prevNum = nums[record];
+    }else{
+      prev = next;
+      prevNum = nextNum; 
+    }
+    count--;
+  }
 };
