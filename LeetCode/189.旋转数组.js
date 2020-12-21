5a608f508bd56b428210ed59255bccdd975fe120
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-21 16:20:45 +0800

    feat: 189-旋转数组

---
+++ LeetCode/189.旋转数组.js
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=javascript
+ *
+ * [189] 旋转数组
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+// 使用一个新数组
+// 完全可以优化的
+const rotate = (nums, k) => {
+  const newOne = [];
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    newOne[(i+k) % len] = nums[i];
+  };
+  for (let i = 0; i < len; i++) {
+    nums[i] = newOne[i];
+  } 
+};
+// @lc code=end
+
+
+// 滚动数组
+const rotate_1 = (nums, k) => {
+  const len = nums.length;
+  for (let i = 0; i < k; i++) {
+    const last = nums[len - 1];
+    for (let j = len-1; j > 0; j--) {
+      nums[j] = nums[j - 1]
+    }
+    nums[0] = last;
+  }
+};
+
+// 滚动数组
+// 简单优化一下，如果 k > length 那就减去
+const rotate_2 = (nums, k) => {
+  const len = nums.length;
+  let l = k % len;
+  for (let i = 0; i < l; i++) {
+    const last = nums[len - 1];
+    for (let j = len-1; j > 0; j--) {
+      nums[j] = nums[j - 1]
+    }
+    nums[0] = last;
+  }
+};
+
+
+// 使用一个新数组
+const rotate_3 = (nums, k) => {
+  const newOne = [];
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    let j = i + k;
+    if(j < len) {
+      newOne[j] = nums[i];
+    }else{
+      while(j >= len) {
+        j -= len;
+      }
+      newOne[j] = nums[i];
+    }
+  };
+  for (let i = 0; i < len; i++) {
+    nums[i] = newOne[i];
+  } 
+};
+
+// 使用一个新数组
+// 完全可以优化的
+const rotate_4 = (nums, k) => {
+  const newOne = [];
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    newOne[(i+k) % len] = nums[i];
+  };
+  for (let i = 0; i < len; i++) {
+    nums[i] = newOne[i];
+  } 
+};
