d74eff748c72eb4be780f1dbf0de0dd88ca9e550
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-22 11:34:30 +0800

    update: 191-位1的个数

--- LeetCode/191.位-1-的个数.js
+++ LeetCode/191.位-1-的个数.js
@@ -11,9 +11,20 @@
  * @return {number}
  */
 
+// 因为 n & (n-1)总是能把n中最低位的1变成0
+const hammingWeight = n =>  {
+  let count = 0;
+  while(n) {
+    count++;
+    n &= (n-1);
+  }
+  return count;
+};
+// @lc code=end
+
 // 因为 n & 1 就是 n 的末尾数
 // 然后 n >> 1 即可
-const hammingWeight = n =>  {
+const hammingWeight_1 = n =>  {
   let count = 0;
   let num = 32;
   while(num) {
@@ -23,5 +34,4 @@ const hammingWeight = n =>  {
   }
   return count;
 };
-// @lc code=end
 
