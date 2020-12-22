f709b4b68579b16e1797c1803fba547c0cc5ce66
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-22 11:32:24 +0800

    feat: 191-位1的个数

---
+++ LeetCode/191.位-1-的个数.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=191 lang=javascript
+ *
+ * [191] 位1的个数 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+
+// 因为 n & 1 就是 n 的末尾数
+// 然后 n >> 1 即可
+const hammingWeight = n =>  {
+  let count = 0;
+  let num = 32;
+  while(num) {
+    if(n & 1) count++;
+    n >>= 1;
+    num--;
+  }
+  return count;
+};
+// @lc code=end
+
