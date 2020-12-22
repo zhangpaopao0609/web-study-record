8ef3b5e3ce3388a1706c346b84d38dd5f882b82c
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-22 12:01:23 +0800

    update: 198-打家劫舍

--- LeetCode/198.打家劫舍.js
+++ LeetCode/198.打家劫舍.js
@@ -10,7 +10,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 状态转移方程
 const rob = nums => {
+  if(!nums || !nums.length ) return 0;
+  const len = nums.length;
+  if(len === 1) return nums[0];
+  const dp = [];
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+
+  for (let i = 2; i < len; i++) {
+    dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+  }
+  return dp[len-1];
+};
+// @lc code=end
+
+// 状态
+const rob_1 = nums => {
   const len = nums.length;
   if(!len) return 0;
   if(len === 1) return nums[0];
@@ -24,5 +41,3 @@ const rob = nums => {
   return dp[len-1] > dp[len-2] ? dp[len-1] : dp[len-2];
 
 };
-// @lc code=end
-
