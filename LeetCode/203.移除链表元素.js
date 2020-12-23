4cddfc5f710b2f08013ab4b200a0911e6671cf55
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-23 15:11:25 +0800

    update: 203-移除链表元素

--- LeetCode/203.移除链表元素.js
+++ LeetCode/203.移除链表元素.js
@@ -18,7 +18,26 @@
  * @param {number} val
  * @return {ListNode}
  */
+// 利用哨兵模式
 const removeElements = (head, val) => {
+  const sentinel = new ListNode(0);
+  sentinel.next = head;
+
+  let prev = sentinel, curr = head;
+  while(curr) {
+    if(curr.val === val) {
+      prev.next = curr.next;
+    }else {
+      prev = curr;
+    }
+    curr = curr.next;
+  }
+  return sentinel.next;
+};
+// @lc code=end
+
+// 指针这种地址引用实在是太神奇了
+const removeElements_1 = (head, val) => {
   while(head && head.val === val) {
     head = head.next; 
   }
@@ -32,5 +51,24 @@ const removeElements = (head, val) => {
   }
   return head;
 };
-// @lc code=end
+// 65/65 cases passed (88 ms)
+// Your runtime beats 99.44 % of javascript submissions
+// Your memory usage beats 36.17 % of javascript submissions (42.6 MB)
 
+
+// 利用哨兵模式
+const removeElements_2 = (head, val) => {
+  const sentinel = new ListNode(0);
+  sentinel.next = head;
+
+  let prev = sentinel, curr = head;
+  while(curr) {
+    if(curr.val === val) {
+      prev.next = curr.next;
+    }else {
+      prev = curr;
+    }
+    curr = curr.next;
+  }
+  return sentinel.next;
+};
