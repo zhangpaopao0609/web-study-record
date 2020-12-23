5d39cc44c32f0bc8de292dc6b7cd5008b8aa1667
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-23 17:09:04 +0800

    update: 205-同构字符串

--- LeetCode/205.同构字符串.js
+++ LeetCode/205.同构字符串.js
@@ -12,6 +12,18 @@
  * @return {boolean}
  */
 
+const isIsomorphic = (s, t) => {
+  const len = s.length;
+  if(!len) return true;
+  for (let i = 0; i < len; i++) {
+    // 返回字符在字符串中第一次出现的索引
+    if(s.indexOf(s.charAt(i)) !== t.indexOf(t.charAt(i))) return false;
+  };
+  return true;
+};
+// @lc code=end
+
+
 // hashMap
 const stringToNum = s => {
   let newS = "";
@@ -26,9 +38,7 @@ const stringToNum = s => {
   return newS;
 }
 
-const isIsomorphic = (s, t) => {
+const isIsomorphic_1 = (s, t) => {
   if(!s.length) return true;
   return stringToNum(s) === stringToNum(t);
 };
-// @lc code=end
-
