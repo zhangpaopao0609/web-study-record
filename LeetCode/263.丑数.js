32273fc153820cc8297189a93bc976685f090ca3
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-25 15:59:29 +0800

    feat: 263.丑数.js

---
+++ LeetCode/263.丑数.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=javascript
+ *
+ * [263] 丑数 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+const isUgly = num => {
+  if(num === 0) return false;
+  while(num % 2 === 0) {
+    num = num / 2;
+  }
+  while(num % 3 === 0) {
+    num = num / 3;
+  }
+  while(num % 5 === 0) {
+    num = num / 5;
+  }
+  return num === 1
+};
+// @lc code=end
+
