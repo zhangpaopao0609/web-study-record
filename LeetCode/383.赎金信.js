d5c5b9229817158239beb77aa0f10ebe3c3c9f0a
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-26 20:00:42 +0800

    update: 383.赎金信.js

--- LeetCode/383.赎金信.js
+++ LeetCode/383.赎金信.js
@@ -11,6 +11,26 @@
  * @param {string} magazine
  * @return {boolean}
  */
+// hash表可以优化一下，可以优化成数组，因为全是小写，所以可以优化为数组
+const canConstruct = (ransomNote, magazine) => {
+  const arr = new Array(26).fill(0);
+  for (let i = 0; i < magazine.length; i++) {
+    arr[magazine.codePointAt(i)-97]++;
+  };
+  for (let i = 0; i < ransomNote.length; i++) {
+    if(arr[ransomNote.codePointAt(i)-97]) {
+      arr[ransomNote.codePointAt(i)-97]--;
+    }else{
+      return false;
+    }
+  };
+  return true;
+};
+// Accepted
+// 129/129 cases passed (88 ms)
+// Your runtime beats 96.55 % of javascript submissions
+// Your memory usage beats 90.93 % of javascript submissions (40.3 MB)
+// @lc code=end
 // 首想hash表
 const canConstruct = (ransomNote, magazine) => {
   const hashMap = new Map();
@@ -33,5 +53,4 @@ const canConstruct = (ransomNote, magazine) => {
   };
   return true;
 };
-// @lc code=end
 
