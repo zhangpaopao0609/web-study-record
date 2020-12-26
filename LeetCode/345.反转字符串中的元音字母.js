9c3eb1574762b68c8d680e434a16ba58168d7ebe
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-26 12:57:53 +0800

    update: 345.反转字符串中的元音字母.js

--- LeetCode/345.反转字符串中的元音字母.js
+++ LeetCode/345.反转字符串中的元音字母.js
@@ -18,6 +18,30 @@ const reverseVowels = s => {
   let end = s.length-1;
   // 不是同时移动，发现了元音就另一边移动，本边暂停
   while(start < end) {
+    if(vowel.has(arrS[start])) {
+      if(vowel.has(arrS[end])) {
+        [arrS[start], arrS[end]] = [arrS[end], arrS[start]];
+        start++;
+      };
+      end--;
+    }else {
+      start++
+    }
+  };
+  return arrS.join('');
+};
+// Accepted
+// 481/481 cases passed (96 ms)
+// Your runtime beats 91.03 % of javascript submissions
+// Your memory usage beats 98.16 % of javascript submissions (42.3 MB)
+// @lc code=end
+const reverseVowels = s => {
+  const vowel = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+  const arrS = s.split('');
+  let start = 0;
+  let end = s.length-1;
+  // 不是同时移动，发现了元音就另一边移动，本边暂停
+  while(start < end) {
     if(!vowel.has(arrS[start])) {
       start++;
     };
@@ -34,5 +58,4 @@ const reverseVowels = s => {
   };
   return arrS.join('');
 };
-// @lc code=end
 
