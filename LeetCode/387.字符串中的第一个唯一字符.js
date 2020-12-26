a47e9e30c3b97091b72727e7ca0b059e651905ee
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-26 20:29:46 +0800

    update: 387.字符串中的第一个唯一字符.js

--- LeetCode/387.字符串中的第一个唯一字符.js
+++ LeetCode/387.字符串中的第一个唯一字符.js
@@ -10,6 +10,27 @@
  * @param {string} s
  * @return {number}
  */
+var firstUniqChar = function(s) {
+  const position = new Map();
+  const q = [];
+  for (let [i, ch] of Array.from(s).entries()) {
+    if (!position.has(ch)) {
+      position.set(ch, i);
+      q.push([s[i], i]);
+    } else {
+      position.set(ch, -1);
+      while (q.length && position.get(q[0][0]) === -1) {
+          q.shift();
+      }
+    }
+  }
+  return q.length ? q[0][1] : -1;
+};
+// 104/104 cases passed (76 ms)
+// Your runtime beats 99.85 % of javascript submissions
+// Your memory usage beats 83.76 % of javascript submissions (40.8 MB)
+// @lc code=end
+
 const firstUniqChar = s => {
   const arr = new Array(26).fill(0);
   const len = s.length;
@@ -21,8 +42,3 @@ const firstUniqChar = s => {
   }
   return -1;
 };
-// 104/104 cases passed (76 ms)
-// Your runtime beats 99.85 % of javascript submissions
-// Your memory usage beats 83.76 % of javascript submissions (40.8 MB)
-// @lc code=end
-
