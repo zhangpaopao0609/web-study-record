600a78cee927f5b4a445e6d44c21ea27694d8eb2
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-30 19:56:44 +0800

    feat: 414-第三大的数

---
+++ LeetCode/414.第三大的数.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=414 lang=javascript
+ *
+ * [414] 第三大的数 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 针对我的第一想法做一点优化
+const thirdMax = nums => {
+  let max1 = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] > max1) max1 = nums[i];
+  };
+  let max2 = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] > max2 && nums[i] !== max1) max2 = nums[i];
+  }
+  let max3 = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] > max3 && nums[i] !== max1 && nums[i] !== max2) max3 = nums[i];
+  };
+  if(max3 !== -Infinity) return max3;
+  return max1;
+};
+// Accepted
+// 26/26 cases passed (76 ms)
+// Your runtime beats 95.83 % of javascript submissions
+// Your memory usage beats 97.24 % of javascript submissions (37.6 MB)
+// @lc code=end
+
+// 第一想法就是最大，然后忽略最大，循环到第三次即可
+const thirdMax = nums => {
+  let max1 = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] > max1) max1 = nums[i];
+  };
+  let max2 = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] > max2 && nums[i] !== max1) max2 = nums[i];
+  }
+  let max3 = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i] > max3 && nums[i] !== max1 && nums[i] !== max2) max3 = nums[i];
+  };
+  if(max3 !== -Infinity) return max3;
+  return max1;
+};
+// Accepted
+// 26/26 cases passed (76 ms)
+// Your runtime beats 95.83 % of javascript submissions
+// Your memory usage beats 97.24 % of javascript submissions (37.6 MB)
