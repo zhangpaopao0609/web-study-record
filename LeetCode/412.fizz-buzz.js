ea4b50180fe828a90aada08ca3c66aadb3ca2a4b
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-30 19:43:31 +0800

    updatE: 412.fizz-buzz.js

--- LeetCode/412.fizz-buzz.js
+++ LeetCode/412.fizz-buzz.js
@@ -10,6 +10,49 @@
  * @param {number} n
  * @return {string[]}
  */
+// 使用hash表来维护一个映射
+const fizzBuzz = n => {
+  const res = new Array();
+  const obj = {
+    3: 'Fizz',
+    5: 'Buzz'
+  }
+  for (let i = 1; i <= n; i++) {
+    let iForStr = "";
+    for (const key in obj) {
+      if(i % key === 0) {
+        iForStr += obj[key];
+      }
+    }
+    if(!iForStr) {
+      iForStr += i.toString();
+    }
+    res.push(iForStr);
+  };
+  return res;
+};
+// @lc code=end
+
+const fizzBuzz = n => {
+  const res = new Array();
+  for (let i = 0; i < n; i++) {
+    const mod15 = (i+1) % 15;
+    const mod3 = (i+1) % 3;
+    const mod5 = (i+1) % 5;
+    if(!mod15) {
+      res.push("FizzBuzz");
+    }else if(!mod3) {
+      res.push("Fizz");
+    }else if(!mod5) {
+      res.push("Buzz");
+    }else{
+      res.push((i+1).toString());
+    }
+  };
+  return res;
+};
+
+
 // 字符串拼接方式
 // 这种方式并不会减少时间复杂度，但是写起来会更加优雅
 const fizzBuzz = n => {
@@ -28,25 +71,6 @@ const fizzBuzz = n => {
       iForStr += i.toString();
     };
     res.push(iForStr);
-  };
-  return res;
-};
-// @lc code=end
-const fizzBuzz = n => {
-  const res = new Array();
-  for (let i = 0; i < n; i++) {
-    const mod15 = (i+1) % 15;
-    const mod3 = (i+1) % 3;
-    const mod5 = (i+1) % 5;
-    if(!mod15) {
-      res.push("FizzBuzz");
-    }else if(!mod3) {
-      res.push("Fizz");
-    }else if(!mod5) {
-      res.push("Buzz");
-    }else{
-      res.push((i+1).toString());
-    }
   };
   return res;
 };
