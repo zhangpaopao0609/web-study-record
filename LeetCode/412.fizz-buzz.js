ddbc497cdafc5c4ef0495a97aefdecb113b5e2f6
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-30 19:36:04 +0800

    updatw: 412.fizz-buzz.js

--- LeetCode/412.fizz-buzz.js
+++ LeetCode/412.fizz-buzz.js
@@ -10,6 +10,28 @@
  * @param {number} n
  * @return {string[]}
  */
+// 字符串拼接方式
+// 这种方式并不会减少时间复杂度，但是写起来会更加优雅
+const fizzBuzz = n => {
+  const res = new Array();
+  for (let i = 1; i <= n; i++) {
+    const divisibleBy3 =  i % 3 === 0;
+    const divisibleBy5 = i % 5 === 0;
+    let iForStr = '';
+    if(divisibleBy3) {
+      iForStr += 'Fizz';
+    }
+    if(divisibleBy5) {
+      iForStr += 'Buzz';
+    }
+    if(!iForStr){
+      iForStr += i.toString();
+    };
+    res.push(iForStr);
+  };
+  return res;
+};
+// @lc code=end
 const fizzBuzz = n => {
   const res = new Array();
   for (let i = 0; i < n; i++) {
@@ -28,5 +50,3 @@ const fizzBuzz = n => {
   };
   return res;
 };
-// @lc code=end
-
