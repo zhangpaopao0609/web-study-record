75b20728fd1921b5a3bacbfa42ffcce63a050f9e
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-31 10:35:20 +0800

    update: 441.排列硬币

--- LeetCode/441.排列硬币.js
+++ LeetCode/441.排列硬币.js
@@ -12,13 +12,28 @@
  */
 // 这种也是可行的
 const arrangeCoins = n => {
-  let i = 1;
-  if(n === 0) return 0;
-  while(n > 0) {
-    n = n - i;
-    i++;
-    if(n < i) return i - 1;
-  }
+  let l = 0;
+  let r = n;
+  while(l <= r) {
+    const m = l + ((r - l) >> 1);
+    const sum = Math.floor((m * (m + 1)) / 2);
+    // const sum = m * (m + 1) >> 1;
+    // 这里要是用二进制的话，js 里面会出现问题。
+    // const a = 1804289383 * 1804289383
+    // console.log(a, a / 2);
+    // console.log(a, a >> 1);
+    // 1627730088803260400  
+    // -984262656
+    console.log(sum, m);
+    if(sum > n) {
+      r = m - 1;
+    }else if(sum < n) {
+      l = m + 1;
+    }else {
+      return m;
+    }
+  };
+  return r;
 };
 // @lc code=end
 // 我觉得吧我这算是完美
@@ -49,4 +64,15 @@ const arrangeCoins = n => {
     }
   };
   return r;
+};
+
+// 这种也是可行的
+const arrangeCoins = n => {
+  let i = 1;
+  if(n === 0) return 0;
+  while(n > 0) {
+    n = n - i;
+    i++;
+    if(n < i) return i - 1;
+  }
 };
