58dfdbd6de65d5f2306244a5b9a323802e645c21
zhangpaopao <zhangpaopao0609@gmail.com>
2020-12-31 10:15:47 +0800

    update: 441.排列硬币

--- LeetCode/441.排列硬币.js
+++ LeetCode/441.排列硬币.js
@@ -10,22 +10,15 @@
  * @param {number} n
  * @return {number}
  */
-// 二分法 也是可以的
+// 这种也是可行的
 const arrangeCoins = n => {
-  let l = 0;
-  let r = n;
-  while(l <= r) {
-    const m = l + ((r - l) >> 1);
-    const sum = m * (m + 1) >> 1;
-    if(sum > n) {
-      r = m - 1;
-    }else if(sum < n) {
-      l = m + 1;
-    }else {
-      return m;
-    }
-  };
-  return r;
+  let i = 1;
+  if(n === 0) return 0;
+  while(n > 0) {
+    n = n - i;
+    i++;
+    if(n < i) return i - 1;
+  }
 };
 // @lc code=end
 // 我觉得吧我这算是完美
