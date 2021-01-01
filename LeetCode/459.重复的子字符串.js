633bee86799340e1980e87ac64a82f98600b774b
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-01 11:36:36 +0800

    update: 459.重复的子字符串.js

--- LeetCode/459.重复的子字符串.js
+++ LeetCode/459.重复的子字符串.js
@@ -10,31 +10,10 @@
  * @param {string} s
  * @return {boolean}
  */
-// 我想，我这个是暴力法吧
-// 对我这个暴力法优化一下
-// 1. 因为子串至少需要重复一遍，因此，只需要一半
-// 2. 因为子串肯定是字符串长度的整数倍
-// 再优化一波暴力法
+// 惊艳一波
 const repeatedSubstringPattern = s => {
-  const len = s.length;
-  for (let i = 1; i * 2 <= len; i++) {
-    if(len % i === 0) {
-      let mark = true;
-      for (let j = i; j < len; j++) {
-        if(s.charAt(j) !== s.charAt(j-i)) {
-          mark = false;
-          break;
-        }
-      };
-      if(mark) return true;
-    }
-  };
-  return false;
+  return (s+s).indexOf(s, 1) !== s.length;
 };
-// Accepted
-// 120/120 cases passed (104 ms)
-// Your runtime beats 45.31 % of javascript submissions
-// Your memory usage beats 38.57 % of javascript submissions (40.9 MB)
 // @lc code=end
 
 
@@ -88,3 +67,29 @@ const repeatedSubstringPattern = s => {
 // 120/120 cases passed (76 ms)
 // Your runtime beats 98.44 % of javascript submissions
 // Your memory usage beats 42.16 % of javascript submissions (40.6 MB)
+
+// 我想，我这个是暴力法吧
+// 对我这个暴力法优化一下
+// 1. 因为子串至少需要重复一遍，因此，只需要一半
+// 2. 因为子串肯定是字符串长度的整数倍
+// 再优化一波暴力法
+const repeatedSubstringPattern = s => {
+  const len = s.length;
+  for (let i = 1; i * 2 <= len; i++) {
+    if(len % i === 0) {
+      let mark = true;
+      for (let j = i; j < len; j++) {
+        if(s.charAt(j) !== s.charAt(j-i)) {
+          mark = false;
+          break;
+        }
+      };
+      if(mark) return true;
+    }
+  };
+  return false;
+};
+// Accepted
+// 120/120 cases passed (104 ms)
+// Your runtime beats 45.31 % of javascript submissions
+// Your memory usage beats 38.57 % of javascript submissions (40.9 MB)
