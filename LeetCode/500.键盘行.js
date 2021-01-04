fe630c721e2f236b32e37c187579aff7dfb1717d
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-04 16:47:02 +0800

    feat: 500.键盘行

--- LeetCode/500.键盘行.js
+++ LeetCode/500.键盘行.js
@@ -10,20 +10,10 @@
  * @param {string[]} words
  * @return {string[]}
  */
-// 现在，我要写一个正则模式
+// 现在，我要写一个正则模式，然后优化一下
 const findWords = words => {
-  const res = [];
-  // const re = /^([qwertyuiop]|[asdfghjkl]|[zxcvbnm])+$/;
-  const re1 = /^[qwertyuiop]+$/;
-  const re2 = /^[asdfghjkl]+$/;
-  const re3 = /^[zxcvbnm]+$/;
-  words.forEach(item => {
-    const copy = item.toLowerCase();
-    if(re1.test(copy) || re2.test(copy) || re3.test(copy)) {
-      res.push(item);
-    }
-  });
-  return res;
+  const re = /(^[qwertyuiop]+$)|(^[asdfghjkl]+$)|(^[zxcvbnm]+$)/;
+  return words.filter(i => re.test(i.toLowerCase()));
 };
 // @lc code=end
 // 这是hash表模式，但是麻烦，所以我没写完
