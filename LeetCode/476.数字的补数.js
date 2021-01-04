8acad91674475569da4bc0f10c50f04548aa1a2f
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-04 12:10:40 +0800

    feat: 476.数字的补数.js

---
+++ LeetCode/476.数字的补数.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=476 lang=javascript
+ *
+ * [476] 数字的补数 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const findComplement = num => {
+  let maxBitNum = 0;
+  let temp = num;
+  while(temp) {
+    maxBitNum++;
+    temp >>= 1;
+  };
+  return num ^ ((1 << maxBitNum) - 1);
+};
+// @lc code=end
+
