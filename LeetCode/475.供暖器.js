e35e37c328c66214d4394a7d5a665e871aa3ebf3
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-04 11:11:58 +0800

    update: 475.供暖器

--- LeetCode/475.供暖器.js
+++ LeetCode/475.供暖器.js
@@ -11,34 +11,40 @@
  * @param {number[]} heaters
  * @return {number}
  */
-// 第一种方式
-// 我是这么想的，将heaters依次从0加到n，直到houses每个数都在变化后的heters中
-// GG 超时了
-const changHeaters = (arr, r) => {
-  const hashSet = new Set();
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = -r; j <= r; j++) {
-      hashSet.add(arr[i] + j);
-    }
-  };
-  return hashSet;
-}
-const findRadius = (houses, heaters) => {
-  let r = -1;
-  while(true) {
-    r++;
-    const nowSet = changHeaters(heaters, r);
-    let flag = true;
-    for (let i = 0; i < houses.length; i++) {
-      if(!nowSet.has(houses[i])) {
-        flag = false;
-        continue;
-      }
+// 要想为每个房子都供暖，其实，每个房子的角度就是希望找到离自己最近的供暖器就行，然后记录下每个房子里供暖器最远一个就行
+// 因此，首先将供暖器排序，然后再利用二分找到每个房子最近的供暖器
+const findNearestHeater = (heaters, house) => {
+  const len = heaters.length;
+  let l = 0, r = len - 1;
+  while(l <= r) {
+    const m = l + ((r - l) >> 1);
+    if(house > heaters[m]) {
+      l = m + 1;
+    }else if(house < heaters[m]) {
+      r = m - 1;
+    }else {
+      return 0;
     };
-    if(flag) {
-      return r;
-    }
+  };
+  if(l === len) {
+    return house - heaters[r];
+  }else if(l === 0) {
+    return heaters[0] - house;
+  }else {
+    return Math.min(house - heaters[l-1], heaters[l] - house);
   }
+}
+
+const findRadius = (houses, heaters) => {
+  let max = 0;
+  heaters = heaters.sort((a, b) => a - b);
+  for (let i = 0; i < houses.length; i++) {
+    const now = findNearestHeater(heaters, houses[i]);
+    if(now > max) {
+      max = now;
+    };
+  };
+  return max;
 };
 // @lc code=end
 
