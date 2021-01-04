61cb5eb3f2f8721d18925368e3b0cb232d6c4879
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-04 16:18:30 +0800

    update: 496.下一个更大元素-i

--- LeetCode/496.下一个更大元素-i.js
+++ LeetCode/496.下一个更大元素-i.js
@@ -11,6 +11,30 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+// 看了题解用单调栈的方法，我觉得暴力法简直弱爆了
+const nextGreaterElement = (nums1, nums2) => {
+  const stack = [], res = [];
+  const hashMap = new Map();
+  for (let i = 0; i < nums2.length; ) {
+    if(stack.length !== 0 && stack[0] < nums2[i]) {
+      hashMap.set(stack.shift(), nums2[i]);
+    } else {
+      stack.unshift(nums2[i]);
+      i++;
+    }
+  };
+  for (let i = 0; i < nums1.length; i++) {
+    if(hashMap.has(nums1[i])) {
+      res.push(hashMap.get(nums1[i]));
+    }else {
+      res.push(-1);
+    }
+  };
+  return res;
+};
+
+// @lc code=end
+
 // 感觉自己就暴力法了
 const nextGreaterElement = (nums1, nums2) => {
   const hashMap = new Map();
@@ -35,5 +59,4 @@ const nextGreaterElement = (nums1, nums2) => {
 // 17/17 cases passed (80 ms)
 // Your runtime beats 96.88 % of javascript submissions
 // Your memory usage beats 39.34 % of javascript submissions (39.7 MB)
-// @lc code=end
 
