b01e0f7d631466db0fbf98f8d7523272c658e081
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-05 10:08:47 +0800

    feat: 501.二叉搜索树中的众数

---
+++ LeetCode/501.二叉搜索树中的众数.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=501 lang=javascript
+ *
+ * [501] 二叉搜索树中的众数 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 中序遍历，左中右，BST的中序排列其实就是升序排列 
+const leftMidRight = (tree, res) => {
+  if(!tree) return;
+  leftMidRight(tree.left, res);
+  res.push(tree.val);
+  leftMidRight(tree.right, res);
+}
+
+const findMode = root => {
+  const res = [];
+  leftMidRight(root, res);
+  let base = null, count = 1;
+  let ans=[], maxCount = 1;
+  for (let i = 0; i < res.length; i++) {
+    if(res[i] === base) {
+      count++;
+    }else {
+      base = res[i];
+      count = 1;
+    };
+    if(count === maxCount) {
+      ans.push(res[i]);
+    }
+    if(count > maxCount) {
+      ans = [res[i]];
+      maxCount = count;
+    };
+  };
+  return ans;
+};
+// @lc code=end
+
