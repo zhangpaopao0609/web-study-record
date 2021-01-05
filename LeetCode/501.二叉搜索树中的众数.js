aa3d8651ee4bd5afd0b338745205fe185d566a98
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-05 10:13:57 +0800

    update: 501.二叉搜索树中的众数

--- LeetCode/501.二叉搜索树中的众数.js
+++ LeetCode/501.二叉搜索树中的众数.js
@@ -17,6 +17,42 @@
  * @return {number[]}
  */
 // 中序遍历，左中右，BST的中序排列其实就是升序排列 
+// 优化一下
+
+const findMode = root => {
+  let base = null, count = 1;
+  let ans=[], maxCount = 1;
+
+  const update = val => {
+    if(val === base) {
+      count++;
+    }else {
+      base = val;
+      count = 1;
+    };
+    if(count === maxCount) {
+      ans.push(val);
+    }
+    if(count > maxCount) {
+      ans = [val];
+      maxCount = count;
+    };
+  }
+
+  const leftMidRight = (tree) => {
+    if(!tree) return;
+    leftMidRight(tree.left);
+    update(tree.val);
+    leftMidRight(tree.right);
+  }
+
+  leftMidRight(root);
+  return ans;
+};
+// @lc code=end
+
+
+// 中序遍历，左中右，BST的中序排列其实就是升序排列 
 const leftMidRight = (tree, res) => {
   if(!tree) return;
   leftMidRight(tree.left, res);
@@ -46,5 +82,3 @@ const findMode = root => {
   };
   return ans;
 };
-// @lc code=end
-
