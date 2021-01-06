8a38e61de0e4621db06b536d6ca7042ff9dab5ec
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-06 19:49:00 +0800

    update: 509.斐波那契数

--- LeetCode/509.斐波那契数.js
+++ LeetCode/509.斐波那契数.js
@@ -10,15 +10,27 @@
  * @param {number} n
  * @return {number}
  */
+// 动态规划
 const fib = n => {
-  if(n === 0) return 0;
-  else if (n === 1) return 1;
-  else return fib(n-1) + fib(n-2);
+  const dp = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i-1] + dp[i-2];
+  };
+  return dp[n];
 };
 // @lc code=end
 const fib = n => {
   if(n === 0) return 0;
   else if (n === 1) return 1;
   else return fib(n-1) + fib(n-2);
+};
+
+// 动态规划
+const fib = n => {
+  const dp = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i-1] + dp[i-2];
+  };
+  return dp[n];
 };
 
