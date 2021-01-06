685aa27cb6b0a1d6b7ca59a2088f9b80a30123a2
zhangpaopao <zhangpaopao0609@gmail.com>
2021-01-06 20:04:57 +0800

    update: 520.检测大写字母

--- LeetCode/520.检测大写字母.js
+++ LeetCode/520.检测大写字母.js
@@ -14,11 +14,7 @@
 // 一、判断ascii是不是都小于97还是只有第一个小于97
 // 二、正则
 const detectCapitalUse = word => {
-  // 全大写
-  const re1 = /^[A-Z]+$/;
-  // 首字母大写 或者全小写
-  const re2 = /^[A-Z]?[a-z]*$/;
-  return re1.test(word) || re2.test(word);
+  return /(^[A-Z]+$)|(^[A-Z]?[a-z]*$)/.test(word);
 };
 // @lc code=end
 
@@ -28,5 +24,9 @@ const detectCapitalUse = word => {
   // 首字母大写 或者全小写
   const re2 = /^[A-Z]?[a-z]*$/;
   return re1.test(word) || re2.test(word);
+};
+
+const detectCapitalUse = word => {
+  return /(^[A-Z]+$)|(^[A-Z]?[a-z]*$)/.test(word);
 };
 
