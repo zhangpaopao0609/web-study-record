c09fcab941f0525b004eac3322f1941544164796
zhangpaopao <zhangpaopao0609@gmail.com>
2021-02-03 15:34:22 +0800

    feat: body-parser

--- NodeJs/13-express-middleware/app.js
+++ NodeJs/13-express-middleware/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 // 配置静态资源目录
 app.use(express.static("static"));  // 内置中间件
 app.set("view engine", "ejs");
+
+// 配置三方中间件
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // 应用级中间件(可用于权限判断)
 app.use((req, res, next) => {
@@ -13,17 +18,26 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  let title = "hello"
-  res.render("index", { title })
+  let title = "首页";
+  res.render("index", { title });
 });
 
-app.get("/news", (req, res) => {
-  let msg = {
-    name: 'arrow', age: 26
-  };
-  let hl = "<h1 style='color: red'>hl</h1>"
-  res.render("news", {...msg, hl})
+app.get("/login", (req, res) => {
+  res.render("login");
 });
+
+app.post("/login", (req, res) => {
+  // 这是获取传输过来的数据的原生方式，流的方式；
+  // let result = "";
+  // req.on("data", chunk => {
+  //   result += chunk;
+  // });
+  // req.on("end", () => {
+  //   res.send(result);
+  // })
+  res.send(req.body)
+});
+
 
 // 错误级中间件，也就是兜底
 app.use((req, res, next) => {
