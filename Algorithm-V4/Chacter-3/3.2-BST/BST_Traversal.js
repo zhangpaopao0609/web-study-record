382be4abab2b7fb0f04474689756b41bb33c1dbc
zhangpaopao <zhangpaopao0609@gmail.com>
2021-03-14 11:31:29 +0800

    feat: BST traversal iteration

--- Algorithm-V4/Chacter-3/3.2-BST/BST_Traversal.js
+++ Algorithm-V4/Chacter-3/3.2-BST/BST_Traversal.js
@@ -1,8 +1,32 @@
 // 中序遍历就是 左 中 右
-function MiddleOrderTraversal(tree) {
+function MiddleOrder_Traversal(tree, res) {
   if(tree == null) return;
-  MiddleOrderTraversal(tree.left);
-  console.log(tree.val);
-  MiddleOrderTraversal(tree.right);
+  MiddleOrder_Traversal(tree.left, res);
+  res.push(tree.val)
+  MiddleOrder_Traversal(tree.right, res);
+  return res;
+};
+
+function MiddleOrderTraversal(tree) {
+  return MiddleOrder_Traversal(tree, [])
 }
 
+// 中序遍历的非递归实现
+function MiddleOrderTraversal_iteration(tree) {
+  if(tree == null) return;
+  const s = [];
+  const res = [];
+  while(s.length || tree) {
+    while(tree) {
+      s.push(tree);
+      tree = tree.left;
+    }
+
+    const now = s.pop();
+    res.push(now.val);
+    tree = now.right;
+  };
+  return res;
+}
+
+module.exports = {MiddleOrderTraversal_iteration, MiddleOrderTraversal};
--- Algorithm-V4/Chacter-3/3.2-BST/BinarySearchTreeAll.js
+++ Algorithm-V4/Chacter-3/3.2-BST/BinarySearchTreeAll.js
@@ -1,3 +1,5 @@
+const { MiddleOrderTraversal_iteration, MiddleOrderTraversal } = require('./BST_Traversal');
+
 // BST 
 class Node {
   constructor(key, val, N) {
@@ -178,6 +180,8 @@ tree = put(tree, 6, 'f');
 tree = put(tree, 7, 'g');
 let h = height(tree);
 console.log(tree);
+console.log("MiddleOrderTraversal",MiddleOrderTraversal(tree));
+console.log("MiddleOrderTraversal_iteration",MiddleOrderTraversal_iteration(tree));
 console.log("height:",h);
 console.log("max:", max(tree));
 console.log("min:", min(tree));
