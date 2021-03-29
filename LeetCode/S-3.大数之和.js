8595b60be7799d7e1844672b84291e5926680d34
zhangpaopao <zhangpaopao0609@gmail.com>
2021-03-29 15:45:57 +0800

    feat: 大数之和2

--- LeetCode/S-3.大数之和.js
+++ LeetCode/S-3.大数之和.js
@@ -4,30 +4,30 @@
 // console.log(sum("9007199254740991", "1229007199254740993443")); 
 // console.log(sum("9007199254740991", "-1229007199254740993443"));
 
-function addStrings(str1, str2) {
+function addStrings(str1, str2) {                 // 两数相加
   let i = str1.length-1;
   let j = str2.length-1;
   let res = '', add = 0;
 
   while(i >= 0 || j >= 0 || add) {
-    let now1 = i < 0 ? 0 : str1.charAt(i) - '0';
-    let now2 = j < 0 ? 0 : str2.charAt(j) - '0';
-    let nowSum =  add + now1 + now2;
-    res = nowSum % 10 + res;
-    add = nowSum > 9 ? 1 : 0;
+    let now1 = i < 0 ? 0 : str1.charAt(i) - '0';  // 当前值 或者 0
+    let now2 = j < 0 ? 0 : str2.charAt(j) - '0';  // 当前值 或者 0
+    let nowSum =  add + now1 + now2;              // 当前值加上进位项
+    res = nowSum % 10 + res;                      // 当前项之和 
+    add = nowSum > 9 ? 1 : 0;                     // 下一次的进位项
     i--;
     j--;
   };
   return res;
 };
 
-function minusStrings(str1, str2) {
-  let i = str1.length-1;
+function minusStrings(str1, str2) {               // 两数相减, 大减去小
+  let i = str1.length-1;                          
   let j = str2.length-1;
   let res = '', minus = 0;
 
   while(i >= 0 || j >= 0) {              
-    let now1 = i < 0 ? 0 : str1.charAt(i) - '0';
+    let now1 = i < 0 ? 0 : str1.charAt(i) - '0';  
     let now2 = j < 0 ? 0 : str2.charAt(j) - '0';
     let nowMinus = now1 - now2 - minus;   
     // 如果 nowMinus 大于等于零，那么减得够
@@ -35,27 +35,27 @@ function minusStrings(str1, str2) {
       res = nowMinus + res;
       minus = 0;
     }else {
-      res = (10 + nowMinus) + res;
+      res = (10 + nowMinus) + res;                // 减不够，拿上一位的
       minus = 1;
     }
     i--;
     j--;
   };
-  while(res[0] === '0') {
+  while(res[0] === '0') {                         // 去掉前面的 0
     res = res.slice(1);
   }
   return res;
 }
 
-function compare(str1, str2) {
+function compare(str1, str2) {                    // 比较大小
   let l1 = str1.length;
   let l2 = str2.length;
 
-  if(l1 > l2) {
+  if(l1 > l2) {                                   
     return true;
   }else if(l1 < l2) {
     return false;
-  }else {
+  }else {                                        // 如果长度相同，那么从前往后依次比较
     let i = 0;
     while(i <= l1) {
       if(str1[i] > str2[i]) {
@@ -84,7 +84,7 @@ function sum(str1, str2) {
       return minusStrings(str2, str1.slice(1));
     }
   }
-  if(str1[0] !== '-' && str2[0] === '-') {    // 说明 str1 为负，str2 为正
+  if(str1[0] !== '-' && str2[0] === '-') {    // 说明 str1 为正，str2 为负
     if(compare(str1, str2.slice(1))) {       // 说明 str1 绝对值大于 str2 绝对值 
       return minusStrings(str1, str2.slice(1));
     }else {
