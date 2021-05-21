76a9f6357f69249d57f73a022f1428b6b04eebc0
zhangpaopao <zhangpaopao0609@gmail.com>
2021-05-21 10:49:18 +0800

    feat: composition

---
+++ Vue/vue3.0-study/18-composition/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ counter }}
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  setup() {
+    const counter = ref(0);
+    const repositories = ref([]);
+
+    watch(counter, () => {
+      console.log(`counter change ${counter.value}`);
+    });
+
+    console.log(repositories.value);
+
+    return {
+      counter,
+      repositories,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
