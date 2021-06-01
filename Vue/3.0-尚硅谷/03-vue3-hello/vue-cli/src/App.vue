d9772df9620bf3357143c43d66ac6fad72a9a7c3
zhangpaopao <zhangpaopao0609@gmail.com>
2021-06-01 17:07:14 +0800

    feat: hellp

---
+++ Vue/3.0-尚硅谷/03-vue3-hello/vue-cli/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- vue2 中必须要有根标签，vue3中可以没有根标签 -->
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  } 
+});
+</script>
+
+<style> 
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
