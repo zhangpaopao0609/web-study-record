dd231ba5f729fbbbbd3b9b079873d3c6e81a2421
zhangpaopao <zhangpaopao0609@gmail.com>
2021-06-03 16:23:51 +0800

    feat: customRef more

--- Vue/3.0-尚硅谷/04-vue3-study/src/15/App.vue
+++ Vue/3.0-尚硅谷/04-vue3-study/src/15/App.vue
@@ -7,15 +7,15 @@
 import { defineComponent, customRef } from 'vue'
 
 // 需求，利用 customRef 实现一个 debounce 方法，（防抖，当触发事件完全结束后delay才执行， 节流是每隔delay执行一次）
-const useCustomRefImplementDebounce = (data: string, delay: number=500, cb: Function=() => {}) => {
+const useCustomRefImplementDebounce = <T>(data: T, delay: number=500, cb: Function=() => {}) => {
   let timeout: number;
   return customRef((track, trigger) => {
     return {
-      get() {
+      get(): T {
         track();
         return data;
       },
-      set(newVal: string) {
+      set(newVal: T) {
         clearTimeout(timeout);
         timeout = setTimeout(() => {
           cb();
@@ -32,7 +32,7 @@ export default defineComponent({
     const asyncHandler = () => setTimeout(() => {
       console.log('接口调用成功！！！！');
     }, 1000);
-    const data = useCustomRefImplementDebounce('hello', 500, asyncHandler);
+    const data = useCustomRefImplementDebounce<string>('hello', 500, asyncHandler);
     return {
       data,
     };
