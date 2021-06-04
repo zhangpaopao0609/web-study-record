5d417782c2e9141922694338cd899a7813083330
zhangpaopao <zhangpaopao0609@gmail.com>
2021-06-04 09:47:11 +0800

    feat: readonly and shallow

--- Vue/3.0-尚硅谷/04-vue3-study/src/18/readonly.js
+++ Vue/3.0-尚硅谷/04-vue3-study/src/18/readonly.js
@@ -1,39 +1,43 @@
-// shallowReadonly 就是只能读取, 设置时提示错误
+// readonly 就是只能读取, 设置时提示错误, 并且是深度的
 
-function shallowReadonly(target) {
-  const shallowReadonlyHandler = {
+function readonly(target) {
+  const readonlyHandler = {
     get(target, key) {
       console.log("获取数据！！");
-      return Reflect.get(target, key);
+      return readonly(Reflect.get(target, key));
     },
     set() {
-      throw new Error("This is shallowReadonly data!!Can not modify it!!");
+      throw new Error("This is readonly data!!Can not modify it!!");
     },
     deleteProperty() {
-      throw new Error("This is shallowReadonly data!!Can not modify it!!");
+      throw new Error("This is readonly data!!Can not delete it!!");
     },
   };
 
   if(target && typeof target === 'object') {
-    return new Proxy(target, shallowReadonlyHandler);
+    return new Proxy(target, readonlyHandler);
   }else {
     return target;
   };
 };
 
-const user_3 = {
+const user_4 = {
   name: 'ardor',
   age: 20,
   wife: {
     name: 'li',
     age: 18,
+    other: [1, 2],
   },
 };
 
-const user_3_shallowReadonly = shallowReadonly(user_3);
-user_3_shallowReadonly.name;
+const user_4_Readonly = readonly(user_4);
+// user_4_Readonly.name;
 
-user_3_shallowReadonly.wife.age += 1;
-console.log(user_3_shallowReadonly.wife.age);
+// user_4_Readonly.wife.other.push(3);
+user_4_Readonly.wife.other[0] = 3;
 
-user_3_shallowReadonly.age += 1;
+// user_4_Readonly.wife.age += 1;
+// console.log(user_4_Readonly.wife.age);
+
+// user_4_Readonly.age += 1;
