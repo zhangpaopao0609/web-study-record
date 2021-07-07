32098deb26d8a84bec55d2b95437c4f15dc3545c
zhangpaopao <zhangpaopao0609@gmail.com>
2021-07-07 20:22:49 +0800

    feat: webpack-dev-middleware

---
+++ Webpack/Webpack-document-guide/04-development/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+
+// 告知 express 使用 webpack-dev-middleware
+// 以及将 webpack.config.js 配置文件作为基础配置
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+}));
+
+// 将文件 serve 到 port 6090
+app.listen(6090, () => {
+  console.log('example app listening on port 6090!!');
+});
--- Webpack/Webpack-document-guide/04-development/webpack.config.js
+++ Webpack/Webpack-document-guide/04-development/webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
 
   plugins: [
