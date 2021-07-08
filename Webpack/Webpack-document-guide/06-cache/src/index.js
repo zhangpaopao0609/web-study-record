7dd73a482acb4148c7138e7fcb1560f1ae540a24
zhangpaopao <zhangpaopao0609@gmail.com>
2021-07-08 14:55:09 +0800

    feat: cache

---
+++ Webpack/Webpack-document-guide/06-cache/src/index.js
@@ -0,0 +1,12 @@
+import _ from "lodash";
+
+function component() {
+  const element = document.createElement('div');
+
+  // lodash 在当前 script 中使用 import 引入
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ Webpack/Webpack-document-guide/06-cache/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    index: './src/index.js',
+  },
+
+  devServer: {
+    port: 6090,
+    open: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Catching',
+    }),
+  ],
+
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
