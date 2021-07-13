f930a8b915ae802e3ff3d3e8bbbbb616ab192826
zhangpaopao <zhangpaopao0609@gmail.com>
2021-07-13 14:34:23 +0800

    feat: add production

---
+++ Webpack/Webpack-document-guide/12-production/webpack.common.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
