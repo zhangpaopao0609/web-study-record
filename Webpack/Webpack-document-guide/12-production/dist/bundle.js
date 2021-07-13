9283e1ffaa534bf565467262bdc361c52dc948f2
zhangpaopao <zhangpaopao0609@gmail.com>
2021-07-13 11:38:07 +0800

    feat: production

---
+++ Webpack/Webpack-document-guide/12-production/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.body.appendChild(function(){const e=document.createElement("pre");return e.innerHTML=["Hello webpack!!","5 cubed is equal to"+(5,125)].join("\n\n"),e}())})();
