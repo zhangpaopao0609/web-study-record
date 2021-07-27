ed7f4f7e10fa3c934cc50530ba195972e26a38ab
zhangpaopao <zhangpaopao0609@gmail.com>
2021-07-27 09:50:43 +0800

    feat: reduce

---
+++ React/shanguigu/react-staging/04-todolist/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Weather from "./views/Weather/index.jsx";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Weather />
+    );
+  };
+};
---
+++ React/shanguigu/react-staging/04-todolist/src/components/AddItem/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddItem extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" name="" id="" />
+      </div>
+    );
+  };
+};
