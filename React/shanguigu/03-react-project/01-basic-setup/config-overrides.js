2936de269a65fccb30fae2365965fb8a2b81d61f
zhangpaopao <zhangpaopao0609@gmail.com>
2021-07-29 09:46:53 +0800

    feat: basic-setup

---
+++ React/shanguigu/03-react-project/01-basic-setup/config-overrides.js
@@ -0,0 +1,15 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: { '@primary-color': '#1DA57A' },
+    }
+  }),
+);
