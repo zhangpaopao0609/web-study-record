107722c3369d5a5feaa9e367695ea82ae58cb5aa
zhangpaopao <zhangpaopao0609@gmail.com>
2021-08-12 10:57:52 +0800

    feat: redux-devtools

--- React/shanguigu/04-redux/05-react-redux/src/redux/index.js
+++ React/shanguigu/04-redux/05-react-redux/src/redux/index.js
@@ -2,12 +2,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 // 引入支持异步 action 的中间件
 import reduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import countReducer from "./count/reducers";
 import personReducer from "./person/reducers";
 
 const store = createStore(combineReducers({
   count: countReducer,
   person: personReducer,
-}), applyMiddleware(reduxThunk));
+}), composeWithDevTools(applyMiddleware(reduxThunk)));
 
 export default store;
