5c77fbf308097ed5a7e1ae8f9af6a16aeec4dc69
zhangpaopao <zhangpaopao0609@gmail.com>
2021-09-03 11:13:34 +0800

    feat: 04-automatic-name-inference

---
+++ Vue/Vue3.2-released/vue3.2-Demo/src/04-automatic-name-inference/Foo.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Automatic Name Inference</h1>
+  <h2>FileName——Foo.vue</h2>
+  <h2>ComponentName——Foo</h2>
+</template>
---
+++ Vue/Vue3.2-released/vue3.2-Demo/src/04-automatic-name-inference/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Automatic Name Inference</h1>
+  <h2>FileName——index.vue</h2>
+  <h2>ComponentName——Index</h2>
+</template>
