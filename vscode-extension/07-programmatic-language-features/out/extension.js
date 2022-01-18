16ee019907b6409404744406f9b02a854828095b
zhangpaopao <zhangpaopao0609@gmail.com>
2022-01-18 16:11:19 +0800

    feat: try more api

--- vscode-extension/07-programmatic-language-features/out/extension.js
+++ vscode-extension/07-programmatic-language-features/out/extension.js
@@ -11,9 +11,95 @@ function activate(context) {
             };
         }
     });
-    vscode.workspace.onDidChangeTextDocument(event => {
-        const uri = event.document.uri;
+    // vscode.languages.registerCodeActionsProvider('javascript', {
+    // 	provideCodeActions(document, position, token) {
+    // 		return [{
+    // 			title: '11',
+    // 			command: '111'
+    // 		}]
+    // 	}
+    // })
+    // vscode.languages.registerDocumentHighlightProvider('javascript', {
+    // 	provideDocumentHighlights(document, position, token) {
+    // 		return [
+    // 			new vscode.DocumentHighlight(new vscode.Range(position, position))
+    // 		]
+    // 	}
+    // })
+    // vscode.languages.registerColorProvider('javascript', {
+    // 	provideDocumentColors(document, token) {
+    // 		return [new vscode.ColorInformation(
+    // 			new vscode.Range(new vscode.Position(1, 1), new vscode.Position(1, 1)),
+    // 			new vscode.Color(0.2, 0.3, 0.5, 1),
+    // 		)]
+    // 	}
+    // })
+    vscode.languages.registerCompletionItemProvider('javascript', {
+        provideCompletionItems(document, position, token, context) {
+            return [
+                new vscode.CompletionItem('11')
+            ];
+        }
+    }, '(');
+    // vscode.languages.registerDeclarationProvider('javascript', {
+    // 	provideDeclaration(document, position, token) {
+    // 		return new vscode.Location(vscode.Uri.file('/out/extension.js'), position)
+    // 	}
+    // });
+    // vscode.languages.registerDocumentFormattingEditProvider('javascript', {
+    // 	provideDocumentFormattingEdits(document, options, token) {
+    // 			return [
+    // 				new vscode.TextEdit(new vscode.Range(
+    // 					new vscode.Position(1, 1),
+    // 					new vscode.Position(1, 1)
+    // 				), '11111')
+    // 			]
+    // 		}
+    // 	});
+    // vscode.languages.registerDocumentHighlightProvider('javascript', {
+    // 	provideDocumentHighlights(document, position, token) {
+    // 			return [
+    // 				new vscode.DocumentHighlight(new vscode.Range(position, position), 2)
+    // 			]
+    // 		}
+    // 	});
+    // vscode.languages.registerDocumentSymbolProvider('javascript', {
+    // 	provideDocumentSymbols(document, token) {
+    // 			return [
+    // 				new vscode.SymbolInformation('1', 14, 'paop', new vscode.Location())
+    // 			]
+    // 		}
+    // 	});
+    // vscode.languages.registerFoldingRangeProvider('javascript', {
+    // 	provideFoldingRanges(document, context, token) {
+    // 			return [
+    // 				new vscode.FoldingRange(1, 2)
+    // 			]
+    // 		}
+    // 	});
+    // vscode.languages.registerReferenceProvider('javascript', {
+    // 	provideReferences(document, position, context, token) {
+    // 			return [new vscode.Location(vscode.Uri.file('./extension.ts'), position)]
+    // 		}
+    // 	});
+    // vscode.languages.registerRenameProvider('javascript', {
+    // 	provideRenameEdits(document, position, newName, token) {
+    // 		return new vscode.WorkspaceEdit()
+    // 	}
+    // });
+    // vscode.languages.registerTypeDefinitionProvider('javascript', {
+    // 	provideTypeDefinition(document, position, token) {
+    // 		return [new vscode.Location(vscode.Uri.file('./extension.ts'), position)]
+    // 	}
+    // });
+    // vscode.languages.setTextDocumentLanguage(vscode.workspace.textDocuments[0], 'typescript');
+    vscode.window.onDidChangeActiveTextEditor((e) => {
+        const a = vscode.window.showInputBox();
+        a.then(res => {
+            console.log(res);
+        });
     });
+    const a = vscode.window.createInputBox();
 }
 exports.activate = activate;
 function deactivate() { }
