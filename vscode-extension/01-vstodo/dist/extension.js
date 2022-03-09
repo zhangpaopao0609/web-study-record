772e7edf00e9c0dfd9f09b426642a147debd62c9
zhangpaopao <zhangpaopao0609@gmail.com>
2022-03-10 07:39:04 +0800

    feat: spa-mono-multi

---
+++ vscode-extension/01-vstodo/dist/extension.js
@@ -0,0 +1,71 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.deactivate = exports.activate = void 0;
+const vscode = __webpack_require__(1);
+function activate(context) {
+    console.log('Congratulations, your extension "01-vstodo" is now active!');
+    context.subscriptions.push(vscode.commands.registerCommand('01-vstodo.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from 01-vstodo!');
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('01-vstodo.askQuestion', async () => {
+        const answear = await vscode.window.showInformationMessage('How was your day?', 'good', 'bad');
+        if (answear === 'bad') {
+            vscode.window.showInformationMessage('Sorry to hear that!!');
+        }
+        else {
+            console.log({ answear });
+        }
+    }));
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
---
+++ vscode-extension/01-vstodo/src/extension.ts
@@ -0,0 +1,30 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "01-vstodo" is now active!');
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('01-vstodo.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World from 01-vstodo!');
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('01-vstodo.askQuestion', async () => {
+			const answear = await vscode.window.showInformationMessage(
+				'How was your day?',
+				'good',
+				'bad',
+			);
+			if (answear === 'bad') {
+				vscode.window.showInformationMessage('Sorry to hear that!!');
+			} else {
+				console.log({ answear });
+			}
+		})
+	);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
