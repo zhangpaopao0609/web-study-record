eeea0f19a2e9c0ebd3ed0a66ae5c27f27fa9d143
zhangpaopao <zhangpaopao0609@gmail.com>
2022-04-19 09:52:38 +0800

    feat: more

---
+++ 零散的知识点/手机传感器/H5-gyroscope/02-deviceorientation-demo-02/index.js
@@ -0,0 +1,45 @@
+const now = document.querySelector(".now");
+const clientWidth = document.documentElement.clientWidth;
+const clientHeight = document.documentElement.clientHeight;
+const body = document.body;
+
+function Horizontal() {
+  const style = `
+    width: ${clientHeight}px;
+    height: ${clientWidth}px;
+    -webkit-transform-origin: ${clientWidth / 2}px ${clientWidth / 2}px;
+    -webkit-transform: rotate(90deg);
+    transform-origin: ${clientWidth / 2}px ${clientWidth / 2}px
+    transform: rotate(90deg); 
+  `;
+  body.setAttribute("style", style);
+}
+
+function Vertical() {
+  const style = `
+    width: 100%;
+    height: 100%;
+    -webkit-transform-origin: 0px 0px;
+    -webkit-transform: rotate(0);
+    transform-origin: 0px 0px
+    transform: rotate(0); 
+  `;
+  body.setAttribute("style", style);
+}
+
+function handleOrientation(event) {
+  const beta = event.beta;
+  const gamma = event.gamma;
+
+  const betaBetween = beta >= -30 && beta <= 30;
+  const gammaBetween = gamma >= -45 && gamma <= 45;
+  if (!gammaBetween && betaBetween) {
+    now.innerHTML = "横屏";
+    Horizontal();
+  } else if (gammaBetween && !betaBetween) {
+    now.innerHTML = "竖屏";
+    Vertical();
+  }
+}
+
+window.addEventListener("deviceorientation", handleOrientation);
