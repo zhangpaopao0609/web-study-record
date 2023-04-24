db33d88567ae2ba1dfe8c74be208fda67b9ef49c
zhangpaopao <zhangpaopao0609@gmail.com>
2023-04-24 12:08:54 +0800

    feat: vitepress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Web Study Record",
-  description: "用于我前端学习的记录, 记录的同时也做一个分享.",
+  title: 'Web Study Record',
+  description: '用于我前端学习的记录, 记录的同时也做一个分享.',
+  srcDir: 'src',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,13 +17,13 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+});
---
+++ docs/src/public/README.md
@@ -0,0 +1,1 @@
+Assets placed in public will be copied to the root of the output directory as-is.
